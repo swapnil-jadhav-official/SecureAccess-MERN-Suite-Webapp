bfaf784fb3e389ba645c5a0f814d8228e66c648f
Swapnil Jadhav <109851868+swapnil-jadhav-official@users.noreply.github.com>
2023-12-10 10:48:22 +0530

    Update helper.js

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -19,7 +19,7 @@ export async function getUsername(){
 export async function authenticate(username){
     try {
         console.log(axios.defaults.baseURL);
-        return await axios.post('${baseURL}/api/authenticate', { username })
+        return await axios.post(`${baseURL}/api/authenticate`, { username })
     } catch (error) {
         return { error : "Username doesn't exist...!"}
     }
@@ -58,7 +58,7 @@ export async function registerUser(credentials){
 export async function verifyPassword({ username, password }){
     try {
         if(username){
-            const { data } = await axios.post('${baseURL}/api/login', { username, password })
+            const { data } = await axios.post(`${baseURL}/api/login`, { username, password })
             return Promise.resolve({ data });
         }
     } catch (error) {
@@ -71,7 +71,7 @@ export async function updateUser(response){
     try {
         
         const token = await localStorage.getItem('token');
-        const data = await axios.put('${baseURL}/api/updateuser', response, { headers : { "Authorization" : `Bearer ${token}`}});
+        const data = await axios.put(`${baseURL}/api/updateuser`, response, { headers : { "Authorization" : `Bearer ${token}`}});
 
         return Promise.resolve({ data })
     } catch (error) {
@@ -82,13 +82,13 @@ export async function updateUser(response){
 /** generate OTP */
 export async function generateOTP(username){
     try {
-        const {data : { code }, status } = await axios.get('${baseURL}/api/generateOTP', { params : { username }});
+        const {data : { code }, status } = await axios.get(`${baseURL}/api/generateOTP`, { params : { username }});
 
         // send mail with the OTP
         if(status === 201){
             let { data : { email }} = await getUser({ username });
             let text = `Your Password Recovery OTP is ${code}. Verify and recover your password.`;
-            await axios.post('/api/registerMail', { username, userEmail: email, text, subject : "Password Recovery OTP"})
+            await axios.post(`${baseURL}/api/registerMail`, { username, userEmail: email, text, subject : "Password Recovery OTP"})
         }
         return Promise.resolve(code);
     } catch (error) {
@@ -99,7 +99,7 @@ export async function generateOTP(username){
 /** verify OTP */
 export async function verifyOTP({ username, code }){
     try {
-       const { data, status } = await axios.get('${baseURL}/api/verifyOTP', { params : { username, code }})
+       const { data, status } = await axios.get(`${baseURL}/api/verifyOTP`, { params : { username, code }})
        return { data, status }
     } catch (error) {
         return Promise.reject(error);
@@ -109,7 +109,7 @@ export async function verifyOTP({ username, code }){
 /** reset password */
 export async function resetPassword({ username, password }){
     try {
-        const { data, status } = await axios.put('${baseURL}/api/resetPassword', { username, password });
+        const { data, status } = await axios.put(`${baseURL}/api/resetPassword`, { username, password });
         return Promise.resolve({ data, status})
     } catch (error) {
         return Promise.reject({ error })
